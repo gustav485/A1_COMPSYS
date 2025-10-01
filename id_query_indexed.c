ee92c9a9168e950e3bc5e049703517ad15b30033
Frederik Rømer <frederik@MacBook-Air-6.local>
2025-10-01 11:02:05 +0200

    Skabelon til indexed fil

--- id_query_indexed.c
+++ id_query_indexed.c
@@ -8,19 +8,33 @@
 
 #include "record.h"
 #include "id_query.h"
-#include "id_query_naive.h"
 
 struct index_record {
     int64_t osm_id;
     const struct record *record;
 };
 
-struct index_data {
-    struct indexed_record *irs;
+struct indexed_data {
+    struct index_record *irs;
     int n;
+};
+
+struct indexed_data* mk_indexed(struct record* rs, int n) {
+  // TODO 
 }
 
-struct indexed_data* mk_indexed(struct record* rs, int n);
-void free_indexed(struct indexed_data* data);
-const struct record* lookup_indexed(struct indexed_data *data, int64_t needle);
+void free_indexed(struct indexed_data* data) {
+  // TODO
+}
+
+const struct record* lookup_indexed(struct indexed_data *data, int64_t needle) {
+  // TODO 
+}
+
+int main(int argc, char** argv) {
+  return id_query_loop(argc, argv,
+                    (mk_index_fn)mk_indexed,
+                    (free_index_fn)free_indexed,
+                    (lookup_fn)lookup_indexed);
+}
 

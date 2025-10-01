6732318f88e6fb273c3f0ea964053ceb8e621a76
Frederik Rømer <frederik@MacBook-Air-6.local>
2025-10-01 10:39:37 +0200

    id_query_naive.c færdig

--- id_query_naive.c
+++ id_query_naive.c
@@ -15,18 +15,30 @@ struct naive_data {
 };
 
 struct naive_data* mk_naive(struct record* rs, int n) {
-  // TODO
-  assert(0);
+  // TODO Wrapper. index 
+  struct naive_data* data = malloc(sizeof(struct naive_data));
+  if (!data){
+    printf(stderr,"Couldn't allocate memory for data");
+    exit(1);
+  }
+  data->rs = rs;
+  data->n = n;
+  return data;
 }
 
 void free_naive(struct naive_data* data) {
-  // TODO
-  assert(0);
+  // TODO kald free på data
+  free(data);
 }
 
 const struct record* lookup_naive(struct naive_data *data, int64_t needle) {
-  // TODO
-  assert(0);
+  // TODO for loop
+  for (int i = 0; i < data->n; i++){
+    if (data->rs[i].osm_id == needle){
+      return &data->rs[i];
+    }
+  }
+  return NULL;
 }
 
 int main(int argc, char** argv) {
@@ -35,3 +47,10 @@ int main(int argc, char** argv) {
                     (free_index_fn)free_naive,
                     (lookup_fn)lookup_naive);
 }
+
+
+//til longitude og latitude
+//Bruge branch if a > b return x hvor x kan være 1, 0, -1
+
+//rs er et array med records 
+//n er antallet af hvor mange der er

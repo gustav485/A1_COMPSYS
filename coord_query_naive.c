8c834352aa9b4b9a83968a0de38303d6f46c47a2
Albertlerhoj <Albertler7@gmail.com>
2025-10-02 16:06:43 +0200

    Update coord_query_naive.c

--- coord_query_naive.c
+++ coord_query_naive.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
@@ -15,23 +16,41 @@ struct naive_data {
 };
 
 struct naive_data* mk_naive(struct record* rs, int n) {
-  assert(0);
-  // TODO
+  // TODO Wrapper. index 
+  struct naive_data* data = malloc(sizeof(struct naive_data));
+  if (!data){
+    printf(stderr,"Couldn't allocate memory for data");
+    exit(1);
+  }
+  data->rs = rs;
+  data->n = n;
+  return data;
 }
 
 void free_naive(struct naive_data* data) {
-  assert(0);
-  // TODO
+  // TODO kald free på data
+  free(data);
 }
 
 const struct record* lookup_naive(struct naive_data *data, double lon, double lat) {
-  assert(0);
-  // TODO
-}
+  // TODO for loop
+  double BestDist = INFINITY;
+  struct record *best = NULL;
+  for (int i = 0; i < data->n; i++){
+    double DifLon = (data->rs[i].lon - lon);
+    double DifLat = (data ->rs[i].lat - lat);
+    double Distance = (sqrt(pow(DifLon, 2)+pow(DifLat, 2)));
 
+      if (Distance < BestDist) {
+        BestDist = Distance;
+        best = &data->rs[i];
+      }
+  }
+  return best;
+}
 int main(int argc, char** argv) {
   return coord_query_loop(argc, argv,
-                          (mk_index_fn)mk_naive,
-                          (free_index_fn)free_naive,
-                          (lookup_fn)lookup_naive);
+                    (mk_index_fn)mk_naive,
+                    (free_index_fn)free_naive,
+                    (lookup_fn)lookup_naive);
 }
